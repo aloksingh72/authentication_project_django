425eb069171f671c3b993822dde1a06fdddd29c6
aloksingh72 <aloksingh36757@gmail.com>
2025-03-07 16:07:41 +0530

    adding middleware in python

--- auth_project/accounts/middleware.py
+++ auth_project/accounts/middleware.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseForbidden
 from django.utils.timezone import now
 
 class LoggingMiddleware:
@@ -17,3 +18,17 @@ class LoggingMiddleware:
     #     response = self.get_response(request)
     #     print(f"[{now()}] Response status: {response.status_code}")
     #     return response
+
+
+# '127.0.0.1'
+BLOCKED_IPS = ['192.168.1.100','10.0.0.50'] # LIST OF BLOCKED IPS
+
+class BlockIPMiddleware:
+    def __init__(self,get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        ip = request.META.get('REMOTE_ADDR') #get the client ip
+        if ip in BLOCKED_IPS:
+            return  ("Your IP is blocked")
+        return self.get_response(request)
